0667f614de638bdabc00220ce5a92156cf3817c4
obengfrancis <130727147+obengfrancis@users.noreply.github.com>
2024-01-26 23:00:44 -0500

    Add files via upload

---
+++ Int_Roman_Numerals.java
@@ -0,0 +1,13 @@
+public class Int_Roman_Numerals {
+    public static void main(String[] args) {
+        String[] symbols = {"I", "V", "X", "L", "C", "D", "M"};
+        int[] values = {1, 5, 10, 50, 100, 500, 1000};
+
+        System.out.println("Roman numerals are represented by seven different symbols: I, V, X, L, C, D, and M.\n");
+        System.out.println("Symbol\tValue");
+
+        for (int i = 0; i < symbols.length; i++) {
+            System.out.println(symbols[i] + "\t" + values[i]);
+        }
+    }
+}
